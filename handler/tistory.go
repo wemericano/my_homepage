3a923bcc68f833e402a530046ce532a3fe17a759
위동균 <dgwi@mysoftwiz.com>
2025-12-19 16:32:19 +0900

    a

--- handler/tistory.go
+++ handler/tistory.go
@@ -1,27 +1,20 @@
 package handler
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-// 티스토리 업로드 API
+// 카카오 로그인 버튼 클릭까지만
 func UploadToTistory(c *gin.Context) {
-	var request struct {
+	var req struct {
 		Title   string `json:"title"`
 		Content string `json:"content"`
 	}
 
-	if err := c.ShouldBindJSON(&request); err != nil {
-		log.Printf("[TISTORY] 요청 바인딩 실패: %v", err)
-		c.JSON(http.StatusBadRequest, gin.H{"code": "FAIL", "message": "잘못된 요청입니다."})
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"code": "FAIL"})
 		return
 	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"code":    "SUCCESS",
-		"message": "티스토리 업로드 요청이 처리되었습니다.",
-	})
 }
